6704e4b15d259adc4dfd20b0175139e60a05893a
David Faure <David.Faure@sbdinc.com>
2022-02-02 08:27:58 +0200

    working api - display as text in app - tests and ci action

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import {render, screen, waitFor} from '@testing-library/react';
 import App from './App';
+import * as testJson from './mocks/test.json';
+import fetch from 'jest-fetch-mock';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+beforeEach(() => {
+    fetch.resetMocks();
+});
+
+
+test('renders the auction cards', async () => {
+
+    fetch.mockResponse(JSON.stringify((testJson as any).default));
+
+    render(<App/>);
+
+    const title = screen.getByText(/React Auction/i);
+
+    expect(title).toBeInTheDocument();
+
+    await waitFor(() => {
+        // Test if we have got the results from the mock API
+        const auctionCard = screen.getByText(/ATH/i);
+        expect(auctionCard).toBeInTheDocument();
+    })
+
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {useDataApi} from './hooks/useDataApi';
+import {Box, makeStyles, Typography} from '@material-ui/core';
+import {Auction} from './models/Auction';
+import {Alert} from '@material-ui/lab';
+
+const useStyles = makeStyles({
+    root: {
+        padding: 16,
+        maxWidth: 1024
+    }
+});
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [data, isLoading, isError]: [Auction[], boolean, boolean] = useDataApi();
+
+    const classes = useStyles();
+
+    return (
+        <Box className={classes.root}>
+            <Typography variant="h2" component="h1">
+                React Auction
+            </Typography>
+
+            {isError && <Alert severity="error">Error joining the server</Alert>}
+
+            {data?.map(auction =>
+                <Typography variant="h3" component="h2" key={auction.id}>
+                    {auction.auctionType} : {auction.inboundId} - {auction.outboundId}
+                </Typography>
+            )}
+
+        </Box>
+    );
 }
 
 export default App;
