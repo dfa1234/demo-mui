2ecf75bea1f34c1f9876f0006eb796494f81ed86
David Faure <David.Faure@sbdinc.com>
2022-02-02 09:09:34 +0200

    Revert "Deploying to master from @ dfa1234/react-auctions@aeedb08859aa3444827b7712ad7193a4827b15f4 🚀"
    This reverts commit 306d47bdc463ac5e4596e6dca77ef539397e47c1.

---
+++ src/App.test.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import {render, screen, waitFor} from '@testing-library/react';
+import App from './App';
+import * as testJson from './mocks/test.json';
+import fetch from 'jest-fetch-mock';
+
+beforeEach(() => {
+    fetch.resetMocks();
+});
+
+
+test('renders the auction cards', async () => {
+
+    fetch.mockResponse(JSON.stringify((testJson as any).default));
+
+    render(<App/>);
+
+    const title = screen.getByText(/React Auction/i);
+
+    expect(title).toBeInTheDocument();
+
+    await waitFor(() => {
+        // Test if we have got the results from the mock API
+        const auctionCard = screen.getByText(/ATH/i);
+        expect(auctionCard).toBeInTheDocument();
+    })
+
+});
